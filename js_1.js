20449e0e154e5bd7d7aa7f0801e853d7f7bccb1b
jayakrishnamarisetty <22A91A61G8@aec.edu.in>
2024-07-16 10:24:24 +0530

    frist commit

---
+++ js_1.js
@@ -0,0 +1,8 @@
+
+        function JK(jk) {
+        // jk.preventDefault();
+        console.log('Name:', document.getElementById('name').value);
+        console.log('Age:', document.getElementById('age').value);
+        console.log('Email:', document.getElementById('email').value);
+        alert('Submitted data');
+        }
